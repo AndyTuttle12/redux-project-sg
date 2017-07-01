b239b1c06d147259152083d0a375aac44f4248ea
Andy Tuttle <andytuttle12@gmail.com>
2017-06-30 20:59:33 -0400

    More practice with action creators, reducers, connect, and conditional rendering.

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
     return [
-        { title: 'Javascript: The Good Parts' },
-        { title: 'Harry Potter and the Goblet of Fire' },
-        { title: 'The Dark Tower' },
-        { title: 'Eloquent Ruby' }
+        { title: 'Javascript: The Good Parts', pages: 101 },
+        { title: 'Harry Potter and the Goblet of Fire', pages: 807 },
+        { title: 'The Dark Tower', pages: 673 },
+        { title: 'Eloquent Ruby', pages: 89 }
     ]
 }
